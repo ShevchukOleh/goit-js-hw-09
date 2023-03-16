cbafee36ad27f0ae8ed032694070ee600d143e64
ShevchukOleh <shew4ukoleg12@gmail.com>
2023-03-16 18:56:27 +0200

    done 01-color-switcher and 02-timer

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const startBtn = document.querySelector('[data-start]'); 
+const stopBtn = document.querySelector('[data-stop]');
 
+startBtn.addEventListener('click', () => {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    changeBgrColor = setInterval(() => {
+        const bgrColor = getRandomHexColor();
+        document.body.style.backgroundColor = bgrColor;
+    }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+    clearInterval(changeBgrColor);
+})
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
