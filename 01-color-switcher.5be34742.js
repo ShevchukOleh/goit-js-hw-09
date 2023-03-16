d222c0c449893e62bb4232d98ffd0afbbaea20fb
ShevchukOleh <115993748+ShevchukOleh@users.noreply.github.com>
2023-03-16 17:39:59 +0000

    Deploying to gh-pages from @ ShevchukOleh/goit-js-hw-09@7648e87f59bb11d97ea8012c6bbca6dafdaf03ea 🚀

--- 01-color-switcher.5be34742.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");t.disabled=!0,e.addEventListener("click",(()=>{e.disabled=!0,t.disabled=!1;try{return changeBgrColor=setInterval((()=>{const e=`#${Math.floor(16777215*Math.random()).toString(16)}`;document.body.style.backgroundColor=e}),1e3)}catch(e){e.message}})),t.addEventListener("click",(()=>{e.disabled=!1,t.disabled=!0;try{clearInterval(changeBgrColor)}catch(e){e.message}}));
-//# sourceMappingURL=01-color-switcher.5be34742.js.map
