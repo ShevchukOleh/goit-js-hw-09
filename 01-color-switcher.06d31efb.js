ad9c079abf4b428fe3c3ca25f43bbf558d02fcfa
ShevchukOleh <115993748+ShevchukOleh@users.noreply.github.com>
2023-03-16 17:06:47 +0000

    Deploying to gh-pages from @ ShevchukOleh/goit-js-hw-09@816c759f1c4c0005c06eb0b5fcaf7d28b7949104 🚀

--- 01-color-switcher.06d31efb.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");e.addEventListener("click",(function(){e.disabled=!0,t.disabled=!1,changeBgrColor=setInterval((function(){var e="#".concat(Math.floor(16777215*Math.random()).toString(16));document.body.style.backgroundColor=e}),1e3)})),t.addEventListener("click",(function(){e.disabled=!1,t.disabled=!0,clearInterval(changeBgrColor)}))}();
-//# sourceMappingURL=01-color-switcher.06d31efb.js.map
---
+++ 01-color-switcher.0e7795ce.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");e.addEventListener("click",(()=>{e.disabled=!0,t.disabled=!1;setInterval((()=>{const e=`#${Math.floor(16777215*Math.random()).toString(16)}`;document.body.style.backgroundColor=e}),1e3)})),t.addEventListener("click",(()=>{e.disabled=!1,t.disabled=!0,clearInterval(changeBgrColor)}));
+//# sourceMappingURL=01-color-switcher.0e7795ce.js.map
