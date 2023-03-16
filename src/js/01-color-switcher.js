7648e87f59bb11d97ea8012c6bbca6dafdaf03ea
ShevchukOleh <shew4ukoleg12@gmail.com>
2023-03-16 19:39:29 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,31 +1,24 @@
-const startBtn = document.querySelector('[data-start]'); 
+const startBtn = document.querySelector('[data-start]');
 const stopBtn = document.querySelector('[data-stop]');
 
-stopBtn.disabled = true;
+function startBackgroundChange() {
+    document.body.style.backgroundColor = getRandomHexColor();
+}
+
+let intervalId;
 
 startBtn.addEventListener('click', () => {
+    intervalId = setInterval(startBackgroundChange, 1000);
     startBtn.disabled = true;
     stopBtn.disabled = false;
-    try {
-        return changeBgrColor = setInterval(() => {
-        const bgrColor = getRandomHexColor();
-        document.body.style.backgroundColor = bgrColor;
-    }, 1000);   
-    } catch (error) {
-        error.message;
-    }
 });
 
 stopBtn.addEventListener('click', () => {
+    clearInterval(intervalId);
     startBtn.disabled = false;
     stopBtn.disabled = true;
-    try {
-       clearInterval(changeBgrColor); 
-    } catch (error) {
-        error.message;
-    }
-})
+});
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
